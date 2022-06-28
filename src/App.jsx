a9d928164c3ab2ea3c90fcd8a1f27413bae45360
Angel Esc <aeo1003@gmail.com>
2022-06-28 20:08:08 +0100

    con captura de errores

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
    const [title, setTitle] = useState('Technology')
    
    const url = 'https://www.reddit.com/r/'+title+'/hot.json?limit=30'
-   const {data,error,loading,thestate} = useFetch(url)
+   const {data,error,loading,status} = useFetch(url)
    
    const [datos,setDatos] = useState([])
 
@@ -43,16 +43,18 @@ function App() {
       if(datos!==null)
         { 
          if (e === 'C') {         
-            const sortedData = JSON.parse(JSON.stringify(data))
-            sortedData.data.children.sort((a,b) => b.data.num_comments - a.data.num_comments); // b - a for reverse sort
+            const sortedData = JSON.parse(JSON.stringify(datos))   //AQUI SE USA DATA!!!
+            sortedData?.data?.children?.sort((a,b) => b.data.num_comments - a.data.num_comments); // b - a for reverse sort
             setDatos(sortedData)
             } 
          
 
          if (e === 'V') {
-            const sortedData = JSON.parse(JSON.stringify(data))
-            sortedData.data.children.sort((a,b) => b.data.ups - a.data.ups); // b - a for reverse sort
-            setDatos(sortedData)         
+            const sortedData = JSON.parse(JSON.stringify(datos))
+             //  if (sortedData?.data?.length>0) {
+                  sortedData?.data?.children?.sort((a,b) => b.data.ups - a.data.ups); // b - a for reverse sort
+                  setDatos(sortedData)         
+              // }
             } 
       }
    }
@@ -61,7 +63,7 @@ function App() {
      <>
          <Context.Provider value={datos}>
             <ButtonAppBar title={title} onChangeSort={(e)=>{sortBy(e)}} onChange={(e) => {cambia(e)} }/>
-            <BlockOfPosts d={datos} e={error} l={loading} t={title}  />
+            <BlockOfPosts d={datos} e={error} l={loading} t={title} s={status} />
          </Context.Provider>
      </>
   )  
